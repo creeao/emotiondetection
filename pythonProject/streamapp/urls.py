1d227460b3b5a97950fd2e1ed94016e282f06829
Bartłomiej Biernat <123biernat123@gmail.com>
2021-05-08 15:34:44 +0200

    deleting and merging

--- pythonProject/streamapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from streamapp import views
-
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('facecam_feed', views.facecam_feed, name='facecam_feed'),
-]
