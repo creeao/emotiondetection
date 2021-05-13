8e136e8cc981d48073792cf8870f98b7271e97fc
BlackBrainFactory <48590849+BlackBrainFacotry@users.noreply.github.com>
2021-05-13 14:57:36 +0200

    Update views.py
    +działa detekcja twarzy na zdjeciu
    
    -trzeba zrobić wyświtlanie zdjęcia
    -problem w odczytaniu zdjecia wybranego przez uzytkownika

--- pythonProject/upload/views.py
+++ pythonProject/upload/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+import cv2,os,urllib.request
+import sys
+face_detection_videocam = cv2.CascadeClassifier(os.path.join(
+			settings.BASE_DIR,'opencv_haarcascade_data/haarcascade_frontalface_default.xml'))
 # Create your views here.
 
 
@@ -10,8 +14,31 @@ def index(request, *args, **kwargs):
 			myfile = request.FILES['myfile']
 			fs = FileSystemStorage()
 			filename = fs.save(myfile.name, myfile)
-			uploaded_file_url = fs.url(filename)
-			return render(request, "upload/home.html", {'uploaded_file_url': uploaded_file_url})
+			# Create the haar cascade
+			faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+			# Read the image
+			image = cv2.imread('C:/Users/G/Desktop/Test.jpg')
+			gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+			# Detect faces in the image
+			faces = faceCascade.detectMultiScale(
+				gray,
+				scaleFactor=1.1,
+				minNeighbors=5,
+				minSize=(30, 30),
+				flags = cv2.CASCADE_SCALE_IMAGE	
+			)
+
+		
+
+			# Draw a rectangle around the faces
+			for (x, y, w, h) in faces:
+				cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+			#cv2.imshow("Faces found", image)
+			
+			return render(request, "upload/home.html", {'uploaded_file_url': "Found {0} faces!".format(len(faces))})
 		else:
 			return render(request, "upload/home.html", {'uploaded_file_url': 'It has to be an image'})
 
