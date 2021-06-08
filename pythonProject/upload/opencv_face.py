5a7a93624f02e256949ec5157a55c67308ab15d7
Bartłomiej Biernat <123biernat123@gmail.com>
2021-06-08 21:21:22 +0200

    feature/Emotions on pic uploaded

--- pythonProject/upload/opencv_face.py
+++ pythonProject/upload/opencv_face.py
@@ -1,4 +1,10 @@
 from django.conf import settings
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import cv2
 import os
@@ -9,21 +15,43 @@ face_detection_videocam = cv2.CascadeClassifier(
     )
 )
 
+model = Sequential()
+
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
 
 def opencv_face(path):
-    img = cv2.imread(path, 1)
+    model.load_weights(os.path.join(
+                settings.BASE_DIR,
+                "opencv_haarcascade_data/model.h5",
+            ))
+    img = cv2.imread(path)
+
+    # dictionary which assigns each label an emotion (alphabetical order)
+    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
     if type(img) is np.ndarray:
         print(img.shape)
 
-        # baseUrl = settings.MEDIA_ROOT_URL + settings.MEDIA_URL
         face_cascade = cv2.CascadeClassifier(
             os.path.join(
                 settings.BASE_DIR,
                 "opencv_haarcascade_data/haarcascade_frontalface_default.xml",
             )
         )
-        # eye_cascade = cv2.CascadeClassifier(baseUrl + "haarcascade_eye.xml")
 
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -32,12 +60,14 @@ def opencv_face(path):
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
             roi_gray = gray[y : y + h, x : x + w]
             roi_color = img[y : y + h, x : x + w]
-            # eyes = eye_cascade.detectMultiScale(roi_gray)
-            # for (ex, ey, ew, eh) in eyes:
-            # cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+            prediction = model.predict(cropped_img)
+            maxindex = int(np.argmax(prediction))
+            cv2.putText(img, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            
 
         cv2.imwrite(path, img)
 
     else:
-        print("someting error")
+        print("something error")
         print(path)
